b3146ebec0a1d272e155dd050c8a14a8b16e25d5
tbdavid2019 <tbdavid2019@gmail.com>
2025-11-25 10:42:24 +0800

    修正 WARMUP_MODEL 環境變數的展開邏輯，以支持 Docker --env-file 的變數展開

--- src/api/setting.py
+++ src/api/setting.py
@@ -22,4 +22,9 @@ WARMUP_INTERVAL_SECONDS = int(os.environ.get("WARMUP_INTERVAL_SECONDS", "600"))
 WARMUP_TIMEZONE = os.environ.get("WARMUP_TIMEZONE", "Asia/Taipei")
 WARMUP_START_HOUR = int(os.environ.get("WARMUP_START_HOUR", "8"))
 WARMUP_END_HOUR = int(os.environ.get("WARMUP_END_HOUR", "18"))
-WARMUP_MODEL = os.environ.get("WARMUP_MODEL", DEFAULT_MODEL)
+# Docker --env-file 不會展開 ${VAR}，這裡做一次簡易展開
+_warmup_model_raw = os.environ.get("WARMUP_MODEL", DEFAULT_MODEL)
+if _warmup_model_raw.strip() in ("${DEFAULT_MODEL}", "$DEFAULT_MODEL"):
+    WARMUP_MODEL = DEFAULT_MODEL
+else:
+    WARMUP_MODEL = _warmup_model_raw
